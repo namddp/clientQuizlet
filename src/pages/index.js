235e4a86443c5b633b59c8ac71d24f6fc8059b41
tranminhwang <j2team.tranminhquang@gmail.com>
2023-05-31 21:31:59 +0700

    use env

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
+  const apiUrl = import.meta.env.VITE_BASE_URL;
   return (
     <>
-      <h1>Hello Exam Client</h1>
+      <h1>Hello Exam Client: {apiUrl}</h1>
     </>
   );
 }

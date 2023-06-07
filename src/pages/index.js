f2ca77bf6674ad651b4bebf4d093ded671b2237d
tranminhwang <j2team.tranminhquang@gmail.com>
2023-06-07 19:49:20 +0700

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Link } from "@chakra-ui/react";
 
 const RegistrationPage = () => {
   const [formData, setFormData] = useState({
@@ -314,12 +315,13 @@ const RegistrationPage = () => {
                 )}
               </div>
               <div className="flex items-center justify-between">
-                <button
+                {/* <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="submit"
                 >
                   Register
-                </button>
+                </button> */}
+                <Button>Register</Button>
                 <a
                   className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                   href="#"

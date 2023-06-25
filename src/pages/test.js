403ecefacd6d67993859bcffe9c1010ffe07a8af
dhuy98 <114129435+dhuy98@users.noreply.github.com>
2023-06-25 14:50:48 +0700

    update create exam

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,15 +1,28 @@
-import React, { useState,useCallback } from 'react';
+import React, { useState, useCallback } from "react";
+import {
+  Box,
+  Button,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+  FormControl,
+  FormLabel,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+} from "@chakra-ui/react";
 
 const QuizCreator = () => {
   const [questions, setQuestions] = useState([]);
-
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
   const handleQuestionChange = (event, questionIndex) => {
     const { name, value } = event.target;
-    setQuestions(prevQuestions => {
+    setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
       updatedQuestions[questionIndex] = {
         ...updatedQuestions[questionIndex],
-        [name]: value
+        [name]: value,
       };
       return updatedQuestions;
     });
@@ -17,7 +30,7 @@ const QuizCreator = () => {
 
   const handleOptionChange = (event, questionIndex, optionIndex) => {
     const { value } = event.target;
-    setQuestions(prevQuestions => {
+    setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
       updatedQuestions[questionIndex] = {
         ...updatedQuestions[questionIndex],
@@ -26,25 +39,25 @@ const QuizCreator = () => {
             return value;
           }
           return option;
-        })
+        }),
       };
       return updatedQuestions;
     });
   };
 
   const handleAddQuestion = () => {
-    setQuestions(prevQuestions => [
+    setQuestions((prevQuestions) => [
       ...prevQuestions,
       {
-        question: '',
-        options: ['', '', '', ''],
-        selectedOption: null
-      }
+        question: "",
+        options: ["", "", "", ""],
+        selectedOption: null,
+      },
     ]);
   };
 
   const handleDeleteQuestion = (questionIndex) => {
-    setQuestions(prevQuestions => {
+    setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
       updatedQuestions.splice(questionIndex, 1);
       return updatedQuestions;
@@ -52,9 +65,11 @@ const QuizCreator = () => {
   };
 
   const handleDeleteOption = (questionIndex, optionIndex) => {
-    setQuestions(prevQuestions => {
+    setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
-      const question = JSON.parse(JSON.stringify(updatedQuestions[questionIndex]));
+      const question = JSON.parse(
+        JSON.stringify(updatedQuestions[questionIndex])
+      );
       question.options.splice(optionIndex, 1);
       updatedQuestions[questionIndex] = question;
       return updatedQuestions;
@@ -62,89 +77,168 @@ const QuizCreator = () => {
   };
 
   const handleAddOption = useCallback((questionIndex) => {
-    setQuestions(prevQuestions => {
+    setQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
       const question = { ...updatedQuestions[questionIndex] };
-      question.options = [...question.options, '']; // Thêm một tùy chọn rỗng mới
+      question.options = [...question.options, ""];
       updatedQuestions[questionIndex] = question;
       return updatedQuestions;
     });
   }, []);
-  
+
+  const handleCorrectOptionChange = (questionIndex, optionIndex) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      updatedQuestions[questionIndex] = {
+        ...updatedQuestions[questionIndex],
+        selectedOption: optionIndex,
+      };
+      return updatedQuestions;
+    });
+  };
 
   const handleFinishQuiz = () => {
-    // Lưu các câu hỏi vào cơ sở dữ liệu hoặc nguồn dữ liệu khác
-    console.log(questions);
+    const hasMissingAnswer = questions.some(question => question.selectedOption === null);
+    if (hasMissingAnswer) {
+      setIsAlertOpen(true);
+    } else {
+      console.log(questions);
+    }
+  };
+
+  const handleCloseAlert = () => {
+    setIsAlertOpen(false);
   };
 
   return (
-    <div className="p-4">
+    <Box p={4}>
       <h2 className="text-2xl font-bold mb-4">Quiz Creator</h2>
 
       {questions.map((question, questionIndex) => (
-        <div key={questionIndex} className="mb-4">
+        <Box key={questionIndex} mb={4}>
           <h3 className="text-lg font-bold">Question {questionIndex + 1}</h3>
-          <button
-            className="py-1 px-2 bg-red-500 text-white rounded"
+          <Button
+            py={1}
+            px={2}
+            bg="red.500"
+            color="white"
+            rounded="md"
             onClick={() => handleDeleteQuestion(questionIndex)}
           >
             Delete
-          </button>
-          <label className="block mt-2">
-            Question:
-            <input
-              className="border border-gray-300 rounded px-2 py-1 mt-1"
+          </Button>
+          <FormControl mt={2}>
+            <FormLabel>Question:</FormLabel>
+            <Input
+              border="1px"
+              borderColor="gray.300"
+              rounded="md"
+              px={2}
+              py={1}
+              mt={1}
               type="text"
               name="question"
               value={question.question}
               onChange={(event) => handleQuestionChange(event, questionIndex)}
             />
-          </label>
+          </FormControl>
           <br />
           {question.options.map((option, optionIndex) => (
-            <div key={optionIndex} className="flex items-center mt-2">
-              <label className="mr-2">
-                Option {String.fromCharCode(65 + optionIndex)}:
-                <input
-                  className="border border-gray-300 rounded px-2 py-1"
+            <Box key={optionIndex} display="flex" alignItems="center" mt={2}>
+              <FormControl mr={2}>
+                <FormLabel>
+                  Option {String.fromCharCode(65 + optionIndex)}:
+                </FormLabel>
+                <Input
+                  border="1px"
+                  borderColor="gray.300"
+                  rounded="md"
+                  px={2}
+                  py={1}
                   type="text"
                   value={option}
-                  onChange={(event) => handleOptionChange(event, questionIndex, optionIndex)}
+                  onChange={(event) =>
+                    handleOptionChange(event, questionIndex, optionIndex)
+                  }
                 />
-              </label>
-              <button
-                className="py-1 px-2 bg-red-500 text-white rounded"
+              </FormControl>
+              <Button
+                py={1}
+                px={2}
+                bg="red.500"
+                color="white"
+                rounded="md"
                 onClick={() => handleDeleteOption(questionIndex, optionIndex)}
               >
                 Delete
-              </button>
-            </div>
+              </Button>
+              <FormControl ml={2}>
+                <FormLabel>Correct Option:</FormLabel>
+                <RadioGroup
+                  value={question.selectedOption}
+                  onChange={() =>
+                    handleCorrectOptionChange(questionIndex, optionIndex)
+                  }
+                >
+                  <Stack direction="row">
+                    <Radio value={optionIndex} />
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+            </Box>
           ))}
-          <button
-            className="py-1 px-2 bg-green-500 text-white rounded mt-2"
+          <Button
+            py={1}
+            px={2}
+            bg="green.500"
+            color="white"
+            rounded="md"
+            mt={2}
             onClick={() => handleAddOption(questionIndex)}
           >
             Add Option
-          </button>
-        </div>
+          </Button>
+        </Box>
       ))}
 
-      <button
-        className="py-2 px-4 bg-green-500 text-white rounded mt-4"
+      <Button
+        py={2}
+        px={4}
+        bg="green.500"
+        color="white"
+        rounded="md"
+        mt={4}
         onClick={handleAddQuestion}
       >
         Add
-      </button>
+      </Button>
 
       {questions.length > 0 && (
-        <button
-          className="py-2 px-4 bg-blue-500 text-white rounded mt-4"
+        <Button
+          py={2}
+          px={4}
+          bg="blue.500"
+          color="white"
+          rounded="md"
+          mt={4}
           onClick={handleFinishQuiz}
         >
           Finish
-        </button>
+        </Button>
       )}
-    </div>
+       <Alert status="error" mt={4} borderRadius="md" display={isAlertOpen ? 'block' : 'none'}>
+        <AlertIcon />
+        <AlertTitle>Vui lòng chọn đáp án đúng cho mỗi câu hỏi!</AlertTitle>
+        <Button
+          ml="auto"
+          variant="outline"
+          size="sm"
+          onClick={handleCloseAlert}
+        >
+          Đóng
+        </Button>
+      </Alert>
+    </Box>
   );
 };
 
